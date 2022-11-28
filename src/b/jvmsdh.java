944ba1dd95328ef1c2b8c2f7a5543fb39f92ca00
calint <calin.tenitchi@gmail.com>
2022-11-28 13:04:04 +0100

    .

--- src/b/jvmsdh.java
+++ src/b/jvmsdh.java
@@ -3,7 +3,7 @@ package b;final class jvmsdh extends Thread{public void run(){
 	if(b.bapp!=null){
 		try{b.bapp.shutdown();}catch(final Throwable e){e.printStackTrace();}
 	}
-	if(b.sessions_save_at_shutdown)){
+	if(b.sessions_save_at_shutdown){
 		try{session.all_save();}catch(final Throwable e){e.printStackTrace();}
 	}
-}
+}}
