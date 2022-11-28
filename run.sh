8783ea91507d25ee96a814a32e57ade3c4563cc7
calint <calin.tenitchi@gmail.com>
2022-11-28 14:06:27 +0100

    .

--- run.sh
+++ run.sh
@@ -3,5 +3,11 @@ set -e
 DIR=$(dirname "$0")
 cd $DIR
 
-cmd="java -cp bin:lib/mysql-connector-java-5.1.49.jar b.b bapp_jdbc_db testdb bapp_jdbc_user c bapp_jdbc_password password bapp_jdbc_ncons 10"
+MYSQL_DB=testdb
+MYSQL_USER=c
+MYSQL_PASSWORD=password
+MYSQL_NCONS=100
+B_NTHREADS=100
+
+cmd="java -cp bin:lib/mysql-connector-java-5.1.49.jar b.b thread_pool_size $B_NTHREADS bapp_jdbc_db $MYSQL_DB bapp_jdbc_user $MYSQL_USER bapp_jdbc_password $MYSQL_PASSWORD bapp_jdbc_ncons $MYSQL_NCONS"
 echo \> $cmd;$cmd
