f97ea6e28155e0b36e49fc4c8b526e1198290b97
calint <calin.tenitchi@gmail.com>
2022-12-02 12:09:04 +0100

    .

--- src/b/sock.java
+++ src/b/sock.java
@@ -4,7 +4,8 @@ import java.nio.channels.SocketChannel;
 import java.util.*;
 public interface sock{
 	enum op{write,read,close,noop,wait}
-	op sockinit(final Map<String,String>hdrs,final SocketChannel sc,final ByteBuffer bbspil)throws Throwable;
+	/** @param bbspil is forwarded by the request to continue processing */
+	op sockinit(Map<String,String>hdrs,SocketChannel sc,ByteBuffer bbspil)throws Throwable;
 	op read()throws Throwable;
 	op write()throws Throwable;
 	void onconnectionlost()throws Throwable;
