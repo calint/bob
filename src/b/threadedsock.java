7df48e22f7cb979a867d45f295db823a29ff57e0
calint <calin.tenitchi@gmail.com>
2022-12-02 12:21:59 +0100

    .

--- src/b/threadedsock.java
+++ src/b/threadedsock.java
@@ -1,1 +1,9 @@
-package b;public interface threadedsock{}
+package b;
+
+/**
+ * Marker interface for sock to be run on own thread instead of the server
+ * thread. If the sock is doing any significant work it should run on own thread
+ * instead of blocking server. If the sock needs access to the req it must run
+ * on own thread and session_id cookie being set.
+ */
+public interface threadedsock{}
