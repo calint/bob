8d09acc8a9a39b4382b7ba187e7eb3629b989667
calint <calin.tenitchi@gmail.com>
2022-12-03 16:14:22 +0100

    .

---
+++ servers-update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+DIR=$(dirname "$0")
+cd $DIR &&
+cat servers-ips.txt | sed -r '/^\s*$/d' | while read IP; do
+	CMD='cd /bob && git pull https://github.com/calint/bob && ./build.sh && systemctl restart bob'
+	echo $IP: $CMD
+	ssh -n root@$IP $CMD
+	echo
+done
