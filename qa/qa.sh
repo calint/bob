05c4d60b6886695a82e47c062b3184aaf501d05a
calint <calin.tenitchi@gmail.com>
2022-12-04 01:12:16 +0100

    .

--- qa/qa.sh
+++ qa/qa.sh
@@ -27,42 +27,42 @@ TEST_NAME="get ranged from cache (range 1-): "
 FILE=x.js
 echo -n $TEST_NAME
 curl -s -r 1- $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp1 res; then exit 1; fi
+if ! cmp -s cmp/1 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged from cache (range -5): "
 FILE=x.js
 echo -n $TEST_NAME
 curl -s -r -5 $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp2 res; then exit 1; fi
+if ! cmp -s cmp/2 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged from cache (range 1-5): "
 FILE=x.js
 echo -n $TEST_NAME
 curl -s -r 1-5 $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp3 res; then exit 1; fi
+if ! cmp -s cmp/3 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged (range 1-): "
 FILE=far_side_dog_ok.jpg
 echo -n $TEST_NAME
 curl -s -r 1- $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp4 res; then exit 1; fi
+if ! cmp -s cmp/4 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged (range -10): "
 FILE=far_side_dog_ok.jpg
 echo -n $TEST_NAME
 curl -s -r -10 $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp5 res; then exit 1; fi
+if ! cmp -s cmp/5 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged (range 1-10): "
 FILE=far_side_dog_ok.jpg
 echo -n $TEST_NAME
 curl -s -r 1-10 $QA_HOST/$URI_FILES/$FILE > res
-if ! cmp -s cmp6 res; then exit 1; fi
+if ! cmp -s cmp/6 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 
