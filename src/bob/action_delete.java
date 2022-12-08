d657da5afd2247cf8f5333fefd144766a8ed6557
calint <calin.tenitchi@gmail.com>
2022-12-08 13:09:40 +0100

    .

--- src/bob/action_delete.java
+++ src/bob/action_delete.java
@@ -15,12 +15,6 @@ public class action_delete extends action{
 		for(String id:selectedIds){
 			table.ls.remove(id);
 		}
-//		for(Iterator<String> i=table.ls.iterator();i.hasNext();){
-//			final String o=i.next();
-//			final String id=o;
-//			if(selectedIds.contains(id))
-//				i.remove();
-//		}
 		selectedIds.clear();
 	}
 }
