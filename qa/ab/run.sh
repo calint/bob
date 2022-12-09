5151b56e8ee68fd9d90e32fa92fbfee6830e5d91
calint <calin.tenitchi@gmail.com>
2022-12-09 13:11:16 +0100

    .

--- qa/ab/run.sh
+++ qa/ab/run.sh
@@ -5,6 +5,14 @@ NREQ=100000
 CON=1000
 URL=http://localhost:8888
 
+echo noname
 ab -n $NREQ -c $CON $URL/ > get-from-cache.txt
 ab -n $NREQ -c $CON $URL/qa/files/far_side_dog_ok.jpg > get-medium-file.txt
 ab -n $NREQ -c $CON $URL/b/test/t1 > dynamic-hello-world.txt
+
+URL=http://localhost
+
+echo nginx
+ab -n $NREQ -c $CON $URL/ > nginx-get-from-cache.txt
+ab -n $NREQ -c $CON $URL/qa/files/far_side_dog_ok.jpg > nginx-get-medium-file.txt
+#ab -n $NREQ -c $CON $URL/b/test/t1 > dynamic-hello-world.txt
