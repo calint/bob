1a38e172d43c0edc325dd76ad6f23e6f64f91178
calint <calin.tenitchi@gmail.com>
2022-12-14 13:34:55 +0100

    .

---
+++ servers-restart-cluster.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+DIR=$(dirname "$0")
+cd $DIR
+
+IPS=$(cat servers-ips.txt | sed -r '/^\s*$/d' | sed -r '/^\s*#/d')
+
+IP=$(set -- $IPS && echo $1)
+CMD='systemctl restart bob-cluster'
+echo $IP: $CMD
+ssh root@$IP "$CMD"
+
+for IP in $IPS; do
+	CMD='systemctl restart bob'
+	echo $IP: $CMD
+	ssh -n root@$IP "$CMD" &
+done
+
+wait
+echo
