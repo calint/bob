73507f5d3958024f723b8bb11f0e396142ead667
calint <calin.tenitchi@gmail.com>
2022-12-15 15:04:29 +0100

    .

--- cluster-install-nodes.sh
+++ cluster-install-nodes.sh
@@ -6,8 +6,8 @@ cd $DIR
 IPS=$(cat cluster-install-nodes.cfg | sed -r '/^\s*$/d' | sed -r '/^\s*#/d')
 
 for IP in $IPS; do
-	scp -o StrictHostKeyChecking=no cluster-node-install.sh root@$IP:/
-	ssh -n root@$IP "cd / && sh cluster-node-install.sh" &
+	scp -o StrictHostKeyChecking=no cluster-on-node-install.sh root@$IP:/
+	ssh -n root@$IP "cd / && sh cluster-on-node-install.sh" &
 done
 
 wait
