10b5237189b48030f07c0d900f81d07daf7b994d
calint <calin.tenitchi@gmail.com>
2022-12-16 00:42:01 +0100

    .

--- qa/slowhttptest/run.sh
+++ qa/slowhttptest/run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
+HOST=http://localhost:8888
+
+CMD="slowhttptest -gotest1 -l600 -c 50000 -X -r 10000 -w 512 -y 1024 -n 5 -z 32 -k 3 -u $HOST/b/test/t4 -p 3"
+echo $CMD
+$CMD
+
 # slow post
-CMD="slowhttptest -XBgoresult -ji=1 -ftext/plain -i10 -c65539 -r65539 -l600 -uhttp://localhost:8888/b/test/t4"
+CMD="slowhttptest -XBgotest2 -ji=1 -ftext/plain -i10 -c50000 -r50000 -l600 -u$HOST/b/test/t4"
 echo $CMD
 $CMD
 
