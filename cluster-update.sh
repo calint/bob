534e067475dba7ea48e8f4a46ab689bce10b2dee
calint <calin.tenitchi@gmail.com>
2022-12-16 09:44:54 +0100

    .

--- cluster-update.sh
+++ cluster-update.sh
@@ -6,7 +6,7 @@ cd $DIR
 IPS=$(cat cluster-ips.txt | sed -r '/^\s*$/d' | sed -r '/^\s*#/d')
 
 for IP in $IPS; do
-	CMD='cd /bob && git stash && git pull https://github.com/calint/bob && ./build.sh'
+	CMD='cd /bob && git add * && git stash && git pull https://github.com/calint/bob && ./build.sh'
 	echo $IP: $CMD
 	ssh -n root@$IP "$CMD" &
 done
