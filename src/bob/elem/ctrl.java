36d40400d4c2717f0de9997f07697bd836e228f3
calint <calin.tenitchi@gmail.com>
2022-12-17 12:32:35 +0100

    .

--- src/bob/elem/ctrl.java
+++ src/bob/elem/ctrl.java
@@ -2,6 +2,10 @@ package bob.elem;
 
 import bob.controller;
 
+/**
+ * The main element in this bapp. The static field
+ * websock_bob.controller_class_name is set to this class name.
+ */
 public class ctrl extends controller {
 	private static final long serialVersionUID = 1L;
 
