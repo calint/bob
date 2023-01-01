038b26a8a9ce1709a27546bef5105fde7684e0ba
calint <calin.tenitchi@gmail.com>
2023-01-01 13:49:46 +0100

    .

--- src/b/x.js
+++ src/b/x.js
@@ -71,7 +71,7 @@ $p=function(eid,txt){
 }
 //$l=function(){if(ui.keys)document.onkeyup=ui.onkey;}
 $a=function(eid,a,v){$(eid).setAttribute(a,v);}
-$r=function(ev,ths,axpb){if(!ev)ev=window.event;$b(ths);if(ev.keyCode!=13)return true;$x(axpb);return false;}
+$r=function(ev,ths,axpb){if(event.keyCode!=13)return true;$x(axpb);return false;}
 $f=function(eid){
 	const e=$(eid);
 	if(!e)return;
