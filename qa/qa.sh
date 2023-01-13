36f322d5f87b3edc155bcea3ca7a04499fefe49b
calint <calin.tenitchi@gmail.com>
2023-01-13 13:14:24 +0100

    .

--- qa/qa.sh
+++ qa/qa.sh
@@ -11,7 +11,7 @@ DIR_CMP=cmp
 
 #--------------------------------------------------------
 TEST_NAME="get small file: "
-FILE=x.css
+FILE=small.css
 echo -n $TEST_NAME
 curl -s $QA_BASE_URL/$URI_FILES/$FILE > res
 if ! cmp -s $DIR_FILES/$FILE res; then exit 1; fi
@@ -25,21 +25,21 @@ if ! cmp -s $DIR_FILES/$FILE res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged from cache (range 1-): "
-FILE=x.js
+FILE=small.js
 echo -n $TEST_NAME
 curl -s -r 1- $QA_BASE_URL/$URI_FILES/$FILE > res
 if ! cmp -s $DIR_CMP/1 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged from cache (range -5): "
-FILE=x.js
+FILE=small.js
 echo -n $TEST_NAME
 curl -s -r -5 $QA_BASE_URL/$URI_FILES/$FILE > res
 if ! cmp -s $DIR_CMP/2 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
 TEST_NAME="get ranged from cache (range 1-5): "
-FILE=x.js
+FILE=small.js
 echo -n $TEST_NAME
 curl -s -r 1-5 $QA_BASE_URL/$URI_FILES/$FILE > res
 if ! cmp -s $DIR_CMP/3 res; then exit 1; fi
@@ -68,7 +68,7 @@ echo " ok"
 #--------------------------------------------------------
 TEST_NAME="chained requests (static content): "
 echo -n $TEST_NAME
-echo -n $'GET /qa/files/x.css HTTP/1.1\r\n\r\nGET /qa/files/x.css HTTP/1.1\r\nConnection: close\r\n\r\n' | nc $QA_HOST $QA_PORT > res
+echo -n $'GET /qa/files/small.css HTTP/1.1\r\n\r\nGET /qa/files/small.css HTTP/1.1\r\nConnection: close\r\n\r\n' | nc $QA_HOST $QA_PORT > res
 if ! cmp -s $DIR_CMP/7 res; then exit 1; fi
 echo " ok"
 #--------------------------------------------------------
