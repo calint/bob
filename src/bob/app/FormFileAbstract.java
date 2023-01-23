d32c6c34da719ab76ea64f0bbf6bcc009e6d137b
calint <calin.tenitchi@gmail.com>
2023-01-23 17:53:13 +0100

    .

---
+++ src/bob/app/FormFileAbstract.java
@@ -0,0 +1,43 @@
+package bob.app;
+
+import java.util.List;
+
+import b.xwriter;
+import bob.FormDbo;
+import bob.Util;
+import db.DbObject;
+import db.test.File;
+
+public abstract class FormFileAbstract extends FormDbo {
+	private static final long serialVersionUID = 1L;
+
+	public FormFileAbstract() {
+		this(null, null, null);
+	}
+
+	public FormFileAbstract(final List<String> idPath, final String objectId, final String initStr) {
+		super(idPath, File.class, objectId, initStr);
+	}
+
+	public String getTitle() {
+		return Util.toStr(getStr(File.name), "New file");
+	}
+
+	@Override
+	protected void render(final xwriter x) throws Throwable {
+		final File o = (File) getObject();
+		beginForm(x);
+		inputText(x, "Name", o, File.name, getInitStr(), "medium");
+		focus(x, File.name);
+		endForm(x);
+	}
+
+	@Override
+	protected void writeToObject(final xwriter x, final DbObject obj) throws Throwable {
+		// FormDbo writes the input fields to the DbObject
+	}
+
+	protected abstract DbObject createObject();
+
+	protected abstract DbObject getObject();
+}
--- src/bob/app/FormUserFile.java
+++ src/bob/app/FormUserFile.java
@@ -2,16 +2,13 @@ package bob.app;
 
 import java.util.List;
 
-import b.xwriter;
-import bob.FormDbo;
-import bob.Util;
 import db.Db;
 import db.DbObject;
 import db.DbTransaction;
 import db.test.File;
 import db.test.User;
 
-public final class FormUserFile extends FormDbo {
+public final class FormUserFile extends FormFileAbstract {
 	private static final long serialVersionUID = 1L;
 
 	public FormUserFile() {
@@ -19,20 +16,7 @@ public final class FormUserFile extends FormDbo {
 	}
 
 	public FormUserFile(final List<String> idPath, final String objectId, final String initStr) {
-		super(idPath, File.class, objectId, initStr);
-	}
-
-	public String getTitle() {
-		return Util.toStr(getStr(File.name), "New file");
-	}
-
-	@Override
-	protected void render(final xwriter x) throws Throwable {
-		final File o = (File) getObject();
-		beginForm(x);
-		inputText(x, "Name", o, File.name, getInitStr(), "medium");
-		focus(x, File.name);
-		endForm(x);
+		super(idPath, objectId, initStr);
 	}
 
 	@Override
@@ -52,10 +36,5 @@ public final class FormUserFile extends FormDbo {
 		final DbTransaction tn = Db.currentTransaction();
 		final User u = (User) tn.get(User.class, idPath.get(0));
 		return (File) u.getFiles().get(oid);
-	}
-
-	@Override
-	protected void writeToObject(final xwriter x, final DbObject obj) throws Throwable {
-		// FormDbo writes the input fields to the DbObject
 	}
 }
