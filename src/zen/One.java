9ea57702ebf1b4edb13301e6474199c4e1a381fb
calint <calin.tenitchi@gmail.com>
2023-05-29 18:17:55 +0200

    .

--- src/zen/One.java
+++ src/zen/One.java
@@ -6,6 +6,6 @@ import b.xwriter;
 public class One extends a {
     @Override
     public void to(xwriter x) throws Throwable {
-        x.p("zen-one ide");
+        x.p("zen-one emulator and integrated development environment");
     }
 }
