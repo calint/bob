6c618f9cbc0190a30dfd992fc877b34feace31e4
calint <calin.tenitchi@gmail.com>
2023-05-30 22:18:58 +0200

    .

--- zen/zen.js
+++ zen/zen.js
@@ -1,9 +1,16 @@
 let zen_run_interval_id = 0;
 let zen_tx_buf = [];
+const fps = 1000 / 60;
 function zen_run_start() {
     if (zen_run_interval_id != 0)
         return;
-    zen_run_interval_id = setInterval(zen_run, 10);
+    zen_run_interval_id = setInterval(zen_tick, fps);
+}
+
+function zen_tick() {
+    if (ui.is_busy)
+        return;
+    $x('- t ' + (zen_tx_buf.length > 0 ? zen_tx_buf.shift() : 0));
 }
 
 function zen_run_stop() {
@@ -16,14 +23,4 @@ function zen_run_stop() {
 function zen_reset() {
     zen_run_stop();
     $x('- rst');
-}
-
-function zen_tick() {
-    if (ui.is_busy)
-        return;
-    $x('- t ' + (zen_tx_buf.length > 0 ? zen_tx_buf.shift() : 0));
-}
-
-function zen_run() {
-    zen_tick();
 }
