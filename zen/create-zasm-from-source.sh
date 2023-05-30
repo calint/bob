719628ba692f00a81df730910fcfec55ea5a0e8a
calint <calin.tenitchi@gmail.com>
2023-05-30 04:30:41 +0200

    .

--- zen/create-zasm-from-source.sh
+++ zen/create-zasm-from-source.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+
 echo "#!/usr/bin/java --source 11" > zasm
 grep -hE "^import" ../src/zen/zasm/* | sort -u >> zasm
 grep -hvE "^import|^package" ../src/zen/zasm/Zasm.java >> zasm
