ae5d3cf7d32f98e1398e7fc9924633c8cae33543
calint <calin.tenitchi@gmail.com>
2024-08-03 20:32:12 +0200

    .

--- src/bob/app/Application.java
+++ src/bob/app/Application.java
@@ -26,7 +26,7 @@ public final class Application implements bapp {
 
 	/** called by b at startup */
 	public void init() throws Throwable {
-//		Db.enable_log=false;
+		Db.enable_log = false;
 		Db.enable_log_sql = false;
 		Db.register(User.class);
 		Db.register(File.class);
@@ -54,20 +54,6 @@ public final class Application implements bapp {
 		b.b.set_path_to_class("/db/test/t1", db.test.t1.class);
 		b.b.set_path_to_class("/db/test/t2", db.test.t2.class);
 		b.b.set_path_to_class("/db/test/t3", db.test.t3.class);
-
-//		db.init("jdbc:mysql://localhost:3306/testdb", "c", "password", 5);
-//		db.init("jdbc:mysql://" + b.bapp_jdbc_host + "/" + b.bapp_jdbc_db
-//				+ "?allowPublicKeyRetrieval=true&useSSL=false", b.bapp_jdbc_user, b.bapp_jdbc_password,
-//				Integer.parseInt(b.bapp_jdbc_ncons));
-//		db.init("jdbc:mysql://" + b.bapp_jdbc_host + "/" + b.bapp_jdbc_db
-//				+ "?ssl-mode=REQUIRED", b.bapp_jdbc_user, b.bapp_jdbc_password,
-//				Integer.parseInt(b.bapp_jdbc_ncons));
-//		db.init("jdbc:mysql://" + b.bapp_jdbc_host + "/" + b.bapp_jdbc_db
-//				+ "?verifyServerCertificate=false&useSSL=true&ssl-mode=REQUIRED", b.bapp_jdbc_user, b.bapp_jdbc_password,
-//				Integer.parseInt(b.bapp_jdbc_ncons));
-
-// verifyServerCertificate=false&useSSL=true&ssl-mode=REQUIRED
-//		ssl-mode=REQUIRED
 	}
 
 	/** called by b at shutdown */
