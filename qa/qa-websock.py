d164e092e6852c93d684af1881824640d52330dd
calint <calin.tenitchi@gmail.com>
2024-08-13 11:11:24 +0200

    .

--- qa/qa-websock.py
+++ qa/qa-websock.py
@@ -7,7 +7,12 @@ import os
 
 
 class WebSocketTest(unittest.TestCase):
-    async def connect_and_test(self):
+    @classmethod
+    def setup_class(cls):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(script_dir)
+
+    async def connect_and_test_small_message(self):
         host = os.getenv('QA_HOST', 'localhost')
         port = os.getenv('QA_PORT', '8888')
         uri = f"ws://{host}:{port}/b/test/ws"
@@ -17,10 +22,27 @@ class WebSocketTest(unittest.TestCase):
             response = await websocket.recv()
             self.assertEqual(message, response)
 
+    async def connect_and_test_large_file(self):
+        host = os.getenv('QA_HOST', 'localhost')
+        port = os.getenv('QA_PORT', '8888')
+        uri = f"ws://{host}:{port}/b/test/ws"
+        async with websockets.connect(uri) as websocket:
+            with open("files/sample.txt", "r") as f:
+                large_message = f.read()
+
+            await websocket.send(large_message)
+            response = await websocket.recv()
+            self.assertEqual(large_message, response)
+
     def test_websocket_echo(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(self.connect_and_test())
+        loop.run_until_complete(self.connect_and_test_small_message())
+
+    def test_websocket_large_file(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.connect_and_test_large_file())
 
 
 if __name__ == "__main__":
