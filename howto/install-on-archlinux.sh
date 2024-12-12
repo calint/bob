db2f1c0497b87ee77570eda351dcd2c5a74eed6e
calint <calin.tenitchi@gmail.com>
2024-12-12 18:37:52 +0700

    .

--- howto/install-on-archlinux.sh
+++ howto/install-on-archlinux.sh
@@ -3,7 +3,7 @@ sudo pacman -S mariadb
 sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo sed -i '/\[mysqld\]/a bind-address=0.0.0.0' /etc/my.cnf.d/server.cnf
 sudo systemctl start mariadb
-sudo mariadb <<EOF
+sudo mariadb << EOF
 create user 'c'@'localhost' identified by 'password';
 create user c identified by 'password';
 create database testdb;
