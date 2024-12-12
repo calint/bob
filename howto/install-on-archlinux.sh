e314cdfc39cc82fcde802c029743a69b1739b67e
calint <calin.tenitchi@gmail.com>
2024-12-12 18:35:56 +0700

    .

---
+++ howto/install-on-archlinux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo pacman -S mariadb
+sudo mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+sudo sed -i '/\[mysqld\]/a bind-address=0.0.0.0' /etc/my.cnf.d/server.cnf
+sudo systemctl start mariadb
+sudo mariadb <<EOF
+create user 'c'@'localhost' identified by 'password';
+create user c identified by 'password';
+create database testdb;
+grant all privileges on testdb.* to 'c'@'localhost';
+grant all privileges on testdb.* to 'c'@'%';
+flush privileges;
+EOF
+
+# note: user 'c' is created both for access from anywhere and localhost
+#       https://mariadb.com/kb/en/troubleshooting-connection-issues/#localhost-and
