752f9a0ba99c4e8feeddd2e36e148a658dd57af2
calint <calin.tenitchi@gmail.com>
2025-04-28 13:09:43 +0700

    .

--- src/db/test/t3.java
+++ src/db/test/t3.java
@@ -12,7 +12,7 @@ public @stateless class t3 extends a {
 
     @Override
     public void to(final xwriter x) throws Throwable {
-        runTest(x, 1, new import_books("/home/c/Downloads/csv-samples/books_data_200k.csv"));
+        runTest(x, 1, new import_books("/home/c/downloads/csv-samples/books_data_200k.csv"));
     }
 
     private void runTest(final xwriter x, final int nruns, final TestCase c) throws Throwable {
