1095953c7aa5cb85b03a03102b77f155028055e3
calint <calin.tenitchi@gmail.com>
2025-05-02 13:26:40 +0700

    .

--- src/bob/Action.java
+++ src/bob/Action.java
@@ -1,21 +1,19 @@
 //
 // reviewed: 2024-08-05
 //           2025-04-28
+//           2025-05-02
 //
 package bob;
 
 import b.a;
 import b.xwriter;
 
+/** Represents an action containing a code. */
 public class Action extends a {
 
     private final static long serialVersionUID = 1;
 
-    private String code;
-
-    public Action() {
-        this("", "");
-    }
+    private final String code;
 
     public Action(final String text, final String code) {
         set(text);
@@ -26,15 +24,13 @@ public class Action extends a {
         return code;
     }
 
-    public final void code(final String str) {
-        code = str;
-    }
-
     @Override
     public final void to(final xwriter x) throws Throwable {
+        // default callback for click
         x.ax(this, null, str(), "act");
     }
 
+    /** Default callback. */
     public final void x_(final xwriter x, final String param) throws Throwable {
         super.bubble_event(x);
     }
